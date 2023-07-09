d260b3da3189bb261534e622645d61a4bae1157f
kedemd <kedemdpc@gmail.com>
2023-07-09 22:09:26 +0300

    Fix: transposition didn't work as expected. Fix: Sometimes the content of the image was cut. Created wrapper around the image and then at the end cut the middle part.

--- augmentor.js
+++ augmentor.js
@@ -21,7 +21,7 @@ const sharp = require('sharp');
  * @property {Object} [backgroundColor={r: 255, g: 255, b: 255}] - The background color to use when applying transformations.
  */
 
- /**
+/**
  * Generates a random number within a range.
  * For negative ranges, the function returns a number between -range and +range.
  * @param {number} range - The range within which to generate the random number.
@@ -31,6 +31,49 @@ function getRandomInRange(range) {
     return Math.random() * range * 2 - range;
 }
 
+function transposeImage(transformedImage, transposeX, transposeY, width, height, backgroundColor) {
+    transposeX = Math.min(transposeX, width);
+    transposeY = Math.min(transposeY, height);
+
+    const newWidth = width - Math.abs(transposeX);
+    const newHeight = height - Math.abs(transposeY);
+
+    if (transposeX >= 0) {
+        // Transpose to right
+        transformedImage = transformedImage
+            .extract({ left: transposeX, top: 0, width: newWidth, height: height })
+            .extend({ top: 0, bottom: 0, left: transposeX, right: 0, background: backgroundColor });
+    } else {
+        // Transpose to left
+        transformedImage = transformedImage
+            .extend({ top: 0, bottom: 0, left: -transposeX, right: 0, background: backgroundColor })
+            .extract({ left: 0, top: 0, width: newWidth, height: height });
+    }
+    if (transposeY >= 0) {
+        // Transpose down
+        transformedImage = transformedImage
+            .extract({ left: 0, top: transposeY, width: newWidth, height: newHeight })
+            .extend({ top: transposeY, bottom: 0, left: 0, right: 0, background: backgroundColor });
+    } else {
+        // Transpose up
+        transformedImage = transformedImage
+            .extend({ top: -transposeY, bottom: 0, left: 0, right: 0, background: backgroundColor })
+            .extract({ left: 0, top: 0, width: newWidth, height: newHeight });
+    }
+
+    return transformedImage;
+}
+
+const cropToCenter = (transformedImage, width, height) => {
+    const centerX = Math.floor(width / 2);
+    const centerY = Math.floor(height / 2);
+    const halfOriginalWidth = Math.floor(width / 2);
+    const halfOriginalHeight = Math.floor(height / 2);
+    const left = Math.max(0, centerX - halfOriginalWidth);
+    const top = Math.max(0, centerY - halfOriginalHeight);
+
+    return transformedImage.extract({ left: left, top: top, width, height }).flatten(true);
+};
 /**
  * Applies various transformations to an image.
  * @async
@@ -52,39 +95,33 @@ async function applyTransformations(imageBuffer, transformationConfig) {
         backgroundColor = {r: 255, g: 255, b: 255, alpha: 1}
     } = transformationConfig;
 
+    // Create a blank canvas
     const sharpImage = sharp(imageBuffer);
     const metadata = await sharpImage.metadata();
     const width = metadata.width;
     const height = metadata.height;
 
-    let transformedImage = sharpImage;
+    // Padding for transformations
+    const padding = Math.max(width, height);
+    const wrapperWidth = width + 2 * padding;
+    const wrapperHeight = height + 2 * padding;
 
-    // Apply transpose transformation
-    if (transposeRange) {
-        const randomTransposeX = getRandomInRange(transposeRange);
-        const randomTransposeY = getRandomInRange(transposeRange);
-        transformedImage = transformedImage.extract({left: randomTransposeX, top: randomTransposeY, width, height});
-    }
+    // Create a blank canvas larger than the image with padding
+    const canvas = sharp({
+        create: {
+            width: wrapperWidth,
+            height: wrapperHeight,
+            channels: 4,
+            background: backgroundColor
+        }
+    }).composite([{
+        input: imageBuffer,
+        top: padding,
+        left: padding
+    }]).toFormat(metadata.format);
 
-    // Resize the image if needed
-    if (zoomRange) {
-        const randomZoomFactor = 1 + Math.random() * zoomRange;
-        const zoomX = Math.round(width * randomZoomFactor);
-        const zoomY = Math.round(height * randomZoomFactor);
-        transformedImage = transformedImage.resize(zoomX, zoomY , { background: backgroundColor });
-    }
-    // Apply shear transformation
-    if (shearRange) {
-        const randomShearX = getRandomInRange(shearRange);
-        const randomShearY = getRandomInRange(shearRange);
-        transformedImage = transformedImage.affine([[1, randomShearX], [randomShearY, 1]], { background: backgroundColor });
-    }
+    let transformedImage = sharp(await canvas.toBuffer());
 
-    // Apply rotation transformation
-    if (rotationRange) {
-        const randomRotationAngle = getRandomInRange(rotationRange);
-        transformedImage = transformedImage.rotate(randomRotationAngle, { background: backgroundColor,  }).resize(width, height,{ background: backgroundColor, fit: 'cover'});
-    }
     if (blurRange) {
         const scalingFactor = 10; // Adjust the scaling factor to control the range
         const minBlurSigma = 0.3; // Minimum value for blur sigma
@@ -121,8 +158,88 @@ async function applyTransformations(imageBuffer, transformationConfig) {
         transformedImage = transformedImage.modulate({contrast: 1 + randomContrast});
     }
 
-    const outputImage = sharp(await transformedImage.toBuffer()).resize(width, height, { fit: "cover" });
-    return outputImage.toBuffer();
+    // Apply shear transformation
+    if (shearRange) {
+        const randomShearX = getRandomInRange(shearRange);
+        const randomShearY = getRandomInRange(shearRange);
+        transformedImage = transformedImage
+            .affine([[1, randomShearX], [randomShearY, 1]], { background: backgroundColor })
+            .resize(wrapperWidth, wrapperHeight);
+
+        transformedImage = cropToCenter(transformedImage, wrapperWidth, wrapperHeight);
+    }
+
+    await transformedImage.toFile('out.png');
+
+    // Apply rotation transformation
+    if (rotationRange) {
+        const randomRotationAngle = getRandomInRange(rotationRange);
+        transformedImage = transformedImage
+            .rotate(randomRotationAngle, { background: backgroundColor })
+            .resize(wrapperWidth, wrapperHeight);
+
+        transformedImage = cropToCenter(transformedImage, wrapperWidth, wrapperHeight);
+    }
+
+    // Apply transpose transformation
+    if (transposeRange) {
+        const randomTransposeX = Math.round((Math.random() - 0.5) * 2 * transposeRange);
+        const randomTransposeY = Math.round((Math.random() - 0.5) * 2 * transposeRange);
+        transformedImage = transposeImage(transformedImage, randomTransposeX, randomTransposeY, wrapperWidth, wrapperHeight, backgroundColor);
+        transformedImage = cropToCenter(transformedImage, wrapperWidth, wrapperHeight);
+    }
+
+    await transformedImage.toFile('out.png');
+
+    // Resize the image if needed
+    if (zoomRange) {
+        transformedImage = sharp(await transformedImage.toBuffer());
+
+        const randomZoomFactor = (Math.random() - 0.5) * 2 * zoomRange;
+        let zoomX, zoomY;
+        if (randomZoomFactor >= 0) {
+            // Zoom in
+            zoomX = Math.round(wrapperWidth * (1 + randomZoomFactor));
+            zoomY = Math.round(wrapperHeight * (1 + randomZoomFactor));
+            transformedImage = transformedImage.resize(zoomX, zoomY , { background: backgroundColor });
+        } else {
+            // Zoom out
+            // First, calculate the scale factor making sure the image is not smaller than the original
+            const scaleFactor = Math.max(1 / (1 + Math.abs(randomZoomFactor)), 1);
+            zoomX = Math.round(wrapperWidth * scaleFactor);
+            zoomY = Math.round(wrapperHeight * scaleFactor);
+            // Next, generate the larger image for the zoom out
+            transformedImage = transformedImage
+                .resize(zoomX, zoomY, { background: backgroundColor })
+                .extend({
+                    top: (zoomY - height) / 2,
+                    bottom: (zoomY - height) / 2,
+                    left: (zoomX - width) / 2,
+                    right: (zoomX - width) / 2,
+                    background: backgroundColor,
+                });
+        }
+    }
+
+    await transformedImage.toFile('out.png');
+
+    transformedImage = sharp(await transformedImage.toBuffer());
+
+    // Extract the center part
+    const tMetadata = await transformedImage.metadata();
+    const centerX = Math.floor(tMetadata.width / 2);
+    const centerY = Math.floor(tMetadata.height / 2);
+    const halfOriginalWidth = Math.floor(width / 2);
+    const halfOriginalHeight = Math.floor(height / 2);
+    const left = Math.max(0, centerX - halfOriginalWidth);
+    const top = Math.max(0, centerY - halfOriginalHeight);
+
+    transformedImage = transformedImage.extract({ left: left, top: top, width, height });
+
+    // Resize the extracted part to the original dimensions
+    transformedImage = transformedImage.resize(width, height, { fit: 'fill' });
+
+    return await transformedImage.toBuffer();
 }
 
 /**
